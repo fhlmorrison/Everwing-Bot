3b80b34f5a794aa72a3cd1940403903fec6a2694
Fraser Morrison <frazemor@hotmail.com>
2022-04-07 19:09:44 -0400

    Added constant.py module to generate coords

---
+++ TODO.md
@@ -0,0 +1,14 @@
+High Prio
+- Set button positions as ratios of the screen size
+- Get positions from xy of top left and bottom right corners
+- Left-right sweeping
+- Standardize coordinate system to avoid using x_pad and y_pad params
+- (attempt to) Substitute all *win32api* and *win32con* calls with *pyautogui* and *keyboard* calls
+- Put constants in own module
+
+- Death detection and resetting
+- Fireball avoidance
+
+
+Low Prio
+- Built in range setter (get xy of corners of game)
---
+++ constant.py
@@ -0,0 +1,32 @@
+# Change these two when using a new setup
+# Values can be found using getPos.py
+TOP_LEFT = (2447, 173)
+BOTTOM_RIGHT = (2934, 1036)
+
+# Screen size derived from above coords
+SCREEN_WIDTH = BOTTOM_RIGHT[0] - TOP_LEFT[0]
+SCREEN_HEIGHT = BOTTOM_RIGHT[1] - TOP_LEFT[1]
+
+# Generates coordinates based on ratio of game screen size
+ratioToCoord = lambda x, y : (int(TOP_LEFT[0] + SCREEN_WIDTH * x), int(TOP_LEFT[1] + SCREEN_HEIGHT * y)) 
+
+# Coordinates of buttons
+PLAY_AGAIN = ratioToCoord(0.500, 0.589)
+CONTINUE_X_BUTTON = ratioToCoord(0.926, 0.114)
+SKIP_BUTTON = ratioToCoord(0.500, 0.963)
+OKAY_BUTTON = ratioToCoord(0.246, 0.890)
+
+# Values of x axis limits on fast character movement mode 
+# +-2 buffer to avoid drift
+LEFT_LIMIT = int(TOP_LEFT[0] + SCREEN_WIDTH * 0.228) + 2
+RIGHT_LIMIT = int(TOP_LEFT[0] + SCREEN_WIDTH * 0.733) - 2
+
+# Gets ratio to game screen size based on coords (used when creating constants)
+getRatio = lambda x, y : ((x - TOP_LEFT[0])/SCREEN_WIDTH, (y - TOP_LEFT[1])/SCREEN_HEIGHT)
+
+def main():
+    print("Debugging mode")
+
+
+if __name__ == '__main__':
+    main()
